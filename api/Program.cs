85877a7c61df9883bb3cb84fd81cd847b820b5fb
Chanakya <ucs3721@outlook.com>
2021-12-26 18:06:03 +0530

    Add cors to api (2)

--- api/Program.cs
+++ api/Program.cs
@@ -1,22 +1,44 @@
 var builder = WebApplication.CreateBuilder(args);
+string _CORS_DEV = "cors-dev";
+string _CORS_PROD = "cors-prod";
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(); // configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(_CORS_DEV, policy =>
+                        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+    options.AddPolicy(_CORS_PROD, policy =>
+    {
+        // TODO: Multiple Origins from env vars
+        string? ALLOWED_ORIGIN = Environment.GetEnvironmentVariable("ALLOWED_ORIGIN");
+
+        if (!string.IsNullOrWhiteSpace(ALLOWED_ORIGIN))
+        {
+            policy.WithOrigins(ALLOWED_ORIGIN).AllowAnyMethod().AllowAnyHeader();
+        }
+        else if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") != "Development")
+        {
+            throw new Exception("No value set for mandatory environment variable ALLOWED_ORIGIN.");
+        }
+    });
+});
 
 var app = builder.Build();
+var env = app.Environment;
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (env.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(env.IsDevelopment() ? _CORS_DEV : _CORS_PROD);
 
 app.UseAuthorization();
 
