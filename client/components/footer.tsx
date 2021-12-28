9ac2300c83730775c6960284360ac12a2885c0c5
Chanakya <ucs3721@outlook.com>
2021-12-28 22:30:25 +0530

    nothing much....

---
+++ client/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <a href="https://vercel.com" target="_blank">
+        Powered by vercel
+      </a>
+    </footer>
+  );
+};
