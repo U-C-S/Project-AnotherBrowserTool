55e1d7a1ee9fe15eced448267e7c3e87bbc1f917
Chanakya <ucs3721@outlook.com>
2021-12-30 23:26:59 +0530

    Add `styled-components` package

--- client/components/common/Header.tsx
+++ client/components/common/Header.tsx
@@ -2,39 +2,53 @@ import React from "react";
 import { Menu } from "../icons";
 
 export const Header = () => {
-  return (
-    <header>
-      <div className="header-logo">
-        <h1>Clink</h1>
-      </div>
+	return (
+		<header>
+			<div className="header-logo">
+				<h1>Clink</h1>
+			</div>
 
-      <div className="header-options-nav">
-        <button>
-          <Menu />
-        </button>
-      </div>
+			<div className="header-options-nav">
+				<button>
+					<Menu />
+				</button>
+			</div>
 
-      <style jsx>{`
-        header {
-          background-color: #282c34;
-          min-height: 80px;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          justify-content: center;
-          font-size: calc(10px + 2vmin);
-          color: white;
-        }
-        h1 {
-          font-size: 40px;
-        }
+			<style jsx>{`
+				.logoDiv,
+				.optsDiv {
+					border-radius: 0 0 10px 10px;
+					background-color: hsl(45, 55%, 90%);
+					filter: drop-shadow(0px 0px 3px hsl(0, 0%, 20%));
+				}
 
-        button {
-          width: 40px;
-          background-color: white;
-          fill: #282c34;
-        }
-      `}</style>
-    </header>
-  );
+				.header {
+					height: 60px;
+					display: flex;
+					justify-content: space-around;
+					h1 {
+						margin: auto;
+						font-size: 40px;
+					}
+				}
+
+				.logoDiv {
+					width: 300px;
+				}
+
+				.optsDiv {
+					width: 300px;
+
+					button {
+						background-color: transparent;
+					}
+
+					button svg {
+						width: 50px;
+						fill: black;
+					}
+				}
+			`}</style>
+		</header>
+	);
 };
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,17 +1,17 @@
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <style global jsx>{`
-        body {
-          margin: 0;
-          padding: 0;
-        }
-      `}</style>
-    </>
-  );
+	return (
+		<>
+			<Component {...pageProps} />
+			<style global jsx>{`
+				body {
+					margin: 0;
+					padding: 0 80px;
+				}
+			`}</style>
+		</>
+	);
 }
 
 export default MyApp;
